d5e75158309925813a6b386caf543c58e8461ae8
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-22 01:09:15 +0300

    Finalizing the app

--- CardWorkoutApp/CardSelectionVC.swift
+++ CardWorkoutApp/CardSelectionVC.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    private let cards = Deck.cards
+    private var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTimer()
+    }
+    
+    
+    @objc private func changeCard() {
+        cardImageView.image = cards.randomElement()
     }
 
-
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(changeCard), userInfo: nil, repeats: true)
+    }
+    
 }
 
---
+++ CardWorkoutApp/RulesVC.swift
@@ -0,0 +1,32 @@
+//
+//  RulesVC.swift
+//  CardWorkoutApp
+//
+//  Created by Ömer Faruk Okumuş on 22.04.2023.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
